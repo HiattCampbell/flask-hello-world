66b4bd884b5bfb4865bce4b3922085221ed49199
Hiatt Campbell <thisishiatt@gmail.com>
2025-07-30 05:46:04 +0000

    Added db_create route

--- app.py
+++ app.py
@@ -7,7 +7,24 @@ def hello_world():
     return 'Hello World from Hiatt in 3308'
 
 @app.route('/db_test')
-def test_db():
+def testing():
     conn = psycopg.connect("postgresql://db_hello_user:7U1ktlvsCHdm5hlqJlSiahJpMo0IX3LV@dpg-d24qnqvgi27c73bb4tgg-a/db_hello")
     conn.close()
     return 'The database is connected'
+
+@app.route('/db_create')
+def creeating():
+    conn = psycopg.connect("postgresql://db_hello_user:7U1ktlvsCHdm5hlqJlSiahJpMo0IX3LV@dpg-d24qnqvgi27c73bb4tgg-a/db_hello")
+    cur = con.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+''')
+    conn.commit()
+    conn.close()
+    return 'Basketball table successfully created'

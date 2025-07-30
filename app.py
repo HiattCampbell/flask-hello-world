ef2ddecf011c5d41d9846e1b4d51df79dc7464df
Hiatt Campbell <thisishiatt@gmail.com>
2025-07-30 05:36:16 +0000

    Updated psycopg

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,6 +8,6 @@ def hello_world():
 
 @app.route('/db_test')
 def test_db():
-    conn = psycopg2.connect("postgresql://db_hello_user:7U1ktlvsCHdm5hlqJlSiahJpMo0IX3LV@dpg-d24qnqvgi27c73bb4tgg-a/db_hello")
+    conn = psycopg.connect("postgresql://db_hello_user:7U1ktlvsCHdm5hlqJlSiahJpMo0IX3LV@dpg-d24qnqvgi27c73bb4tgg-a/db_hello")
     conn.close()
     return 'The database is connected'
